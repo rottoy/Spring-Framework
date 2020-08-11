a02a520cc3594a1ec03e70237d8fbbcff1a6ea34
JeonHuiJae <rottoy@konkuk.ac.kr>
2020-08-11 19:11:57 +0900

    Define Beans and Dependency Injection without XML configuration

---
+++ spring-demo-annotation/src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,22 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SwimJavaConfigDemoApp {
+    public static void main(String[] args){
+
+        //read spring configuration file
+        AnnotationConfigApplicationContext context=
+                new AnnotationConfigApplicationContext(SportConfig.class);
+
+        //get the bean from spring container
+        Coach theCoach = context.getBean("swimCoach",Coach.class);
+
+        //call a method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        System.out.println(theCoach.getDailyFortune());
+        //context close
+        context.close();
+    }
+}
