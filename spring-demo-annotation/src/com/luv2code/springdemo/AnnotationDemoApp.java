d072487c660583ba059c6f65c1ea5e33aa7e0f63
JeonHuiJae <rottoy@konkuk.ac.kr>
2020-08-11 14:30:07 +0900

    Dependency Injection using Autowired annotations

--- spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -3,7 +3,7 @@ package com.luv2code.springdemo;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AnnotationDemoApp {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         //read spring configuration file
         ClassPathXmlApplicationContext context=
@@ -15,7 +15,7 @@ public class AnnotationDemoApp {
         //call a method on the bean
         System.out.println(theCoach.getDailyWorkout());
 
-
+        System.out.println(theCoach.getDailyFortune());
         //context close
         context.close();
     }
--- spring-demo-annotation/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,33 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyClass")
 @Component
 public class TennisCoach implements Coach{
 
+    @Autowired
+    private FortuneService fortuneService;
+
+    //define a constructor
+//    @Autowired
+//    TennisCoach(FortuneService fortuneService){
+//        this.fortuneService = fortuneService;
+//    }
+
+    //define a setter method
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService){
+//        this.fortuneService = fortuneService;
+//    }
     @Override
     public String getDailyWorkout() {
         return "Practice my SpringFramework";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
