8c9a2cb48c7ee3e76627bcc0c8ec38e98f38b321
rottoy <wnsgur9609@naver.com>
2020-08-25 13:22:07 +0900

    hibernate 일대일 단방향 앙방향 관계 cascading 적용

---
+++ hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -0,0 +1,52 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+    public static void main(String[] args) {
+
+        //create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        //create session
+        Session session = factory.getCurrentSession();
+        //use the session object to save Java Object
+        try{
+            //create the objects
+            Instructor tempInstructor=
+                    new Instructor("Chad","Darby","www@naver.com");
+            InstructorDetail tempInstructorDetail=
+                    new InstructorDetail("http://www.luv2code.com/youtube","Luv 2 code!!!");
+
+            //associate the objects
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+            //start a transaction
+            session.beginTransaction();
+
+            // save the instructor
+            //
+            // Note : this will ALSO save the details object
+            // because of CascadeType.ALL
+            //
+            System.out.println("Saving instructor : "+tempInstructor);
+            session.save(tempInstructor);
+
+            //commit the transaction
+            session.beginTransaction().commit();
+        }
+        finally {
+            factory.close();
+        }
+
+    }
+}
